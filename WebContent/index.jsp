b125bd59403ab1589a09578dcfa5e56c0387a264
Rakesh Kapri <rakeshkapri@gmail.com>
2015-02-05 23:44:49 +0530

    Added Login configuration and Jsp Files

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>This is the Index Page Of MindBlogging Application</h2>
+<jsp:forward page=""></jsp:forward>
 </body>
 </html>
---
+++ src/main/com/controller/LoginController.java
@@ -0,0 +1,20 @@
+package main.com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+ 
+	@RequestMapping(method = RequestMethod.POST)
+	public String displayLogin(ModelMap model) {
+ 
+		model.addAttribute("message", "Display Login Page");
+		return "loginPage";
+ 
+	}
+ 
+}
